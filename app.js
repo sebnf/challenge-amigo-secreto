6cb04c4962c40c2de5e4f6516031d69fcc0b5839
Seb <sebastian.flores.acevedo@gmail.com>
2025-08-03 21:51:11 -0400

    Se agrega función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+
+function agregarAmigo(){
+
+    //Capturar nombre amigo secreto en la entrada texto
+    let nombreIngresado = document.querySelector("#amigo").value;
+
+    console.log(`Nombre ingresado: ${nombreIngresado}`);
+
+    //Mostrar alerta si no se ingresó un nombre, si no, actualizar array nombres
+    if (nombreIngresado === ''){
+        alert('Ingresa un nombre válido');
+    } else {
+        nombres.push(nombreIngresado);
+        console.log(`Nombres en array: ${nombres}`);
+    }
+    //Limpiar campo de entrada
+    document.getElementById("amigo").value = '';
+}
